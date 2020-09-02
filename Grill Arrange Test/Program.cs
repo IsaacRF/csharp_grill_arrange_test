1436b1080ba2081d31ad37c6d5b6f47760823c56
Isaac R.F <isaacrf239@gmail.com>
2020-09-02 18:16:03 +0200

    Adds arrange algorithm and view logic
    - Also asks user input for grill size

--- Grill Arrange Test/Program.cs
+++ Grill Arrange Test/Program.cs
@@ -7,11 +7,38 @@ using System.Threading.Tasks;
 
 namespace Grill_Arrange_Test
 {
+    /// <summary>
+    /// Main program class
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// App entry point
+        /// </summary>
+        /// <param name="args">Launch args</param>
         static void Main(string[] args)
         {
-            GrillViewModel grill = new GrillViewModel(20, 30);
+            int grillLength = 20;
+            int grillWidth = 30;
+            bool validLength = false;
+            bool validWidth = false;
+
+            while (!validLength)
+            {
+                Console.WriteLine("Enter a valid Grill Length");
+                validLength = int.TryParse(Console.ReadLine(), out grillLength);
+            }
+            while (!validWidth)
+            {
+                Console.WriteLine("Enter a valid Grill Width");
+                validWidth = int.TryParse(Console.ReadLine(), out grillWidth);
+            }
+           
+            Console.WriteLine("Creating a {0}x{1} grill...", grillLength, grillWidth);
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine();
+
+            GrillViewModel grill = new GrillViewModel(grillLength, grillWidth);
             grill.CookMenus();
             Console.ReadKey();
         }
--- Grill Arrange Test/ViewModels/GrillViewModel.cs
+++ Grill Arrange Test/ViewModels/GrillViewModel.cs
@@ -12,10 +12,8 @@ namespace Grill_Arrange_Test
     /// </summary>
     class GrillViewModel
     {
-        /// <summary>
-        /// Stores grill space state
-        /// </summary>
-        public int[][] grillSpace { get; set; }
+        public int GrillLength { get; set; }
+        public int GrillWidth { get; set; }
 
         /// <summary>
         /// Base constructor
@@ -23,23 +21,9 @@ namespace Grill_Arrange_Test
         /// <param name="grillLength">Grill length in cm</param>
         /// <param name="grillWidth">Grill width in cm</param>
         public GrillViewModel(int grillLength, int grillWidth)
-        {           
-            InitializeGrill(grillLength, grillWidth);
-        }
-
-        /// <summary>
-        /// Initializes grill with specified size
-        /// </summary>
-        /// <param name="grillLength">Grill length in cm</param>
-        /// <param name="grillWidth">Grill width in cm</param>
-        private void InitializeGrill(int grillLength, int grillWidth)
         {
-            grillSpace = new int[grillLength][];
-            //Initialize grill space
-            for (int i = 0; i < grillSpace.Length; i++)
-            {
-                grillSpace[i] = new int[grillWidth];
-            }
+            GrillLength = grillLength;
+            GrillWidth = grillWidth;
         }
 
         /// <summary>
@@ -52,79 +36,56 @@ namespace Grill_Arrange_Test
 
             foreach (var menu in results.OrderBy(r => r.Menu))
             {
-                // Cook the menu
-            }
-        }
+                int itemsReady = 0;
+                int rounds = 0;
+                string roundSummary;
 
-        /// <summary>
-        /// Checks if there is enough remaining space in the grill for specified item size
-        /// </summary>
-        /// <param name="length">Item length</param>
-        /// <param name="width">Item width</param>
-        /// <returns>True if there is space</returns>
-        /// TODO: Need to return WHERE the available space is located rather than true or false
-        public bool IsThereRoom(int length, int width)
-        {
-            List<int> rowsWithSpace = new List<int>();
-            List<int> colsWithSpace = new List<int>();
+                Console.WriteLine("[{0}]", menu.Menu);
 
-            //Search for space horizontally
-            for (int i = 0; i < grillSpace.Length; i++)
-            {
-                bool isRoomInRow = grillSpace[0].Where(cell => (cell == 0)).Count() >= length;
-                if (isRoomInRow)
+                // Round
+                while (itemsReady < menu.Items.Count)
                 {
-                    rowsWithSpace.Add(i);
+                    rounds++;
+                    int grillAvailableArea = GrillLength * GrillWidth;
+                    roundSummary = string.Format("--> Round {0}: | ", rounds);
+
+                    // Items Check
+                    foreach (var item in menu.Items.OrderBy(i => i.Length * i.Width))
+                    {
+                        if (item.Quantity != 0)
+                        {
+                            int itemArea = Convert.ToInt16(Math.Truncate((Double)(item.Length.Value * item.Width.Value)));
+
+                            if (itemArea <= grillAvailableArea)
+                            {
+                                int itemsFitting = grillAvailableArea / itemArea;
+                                itemsFitting = (itemsFitting > item.Quantity) ? item.Quantity.Value : itemsFitting;
+
+                                grillAvailableArea -= itemArea * itemsFitting;
+                                item.Quantity -= itemsFitting;
+                                if (itemsFitting != 0)
+                                {
+                                    roundSummary += string.Format("'{0}' x {1} |", item.Name, itemsFitting);
+                                }
+
+                                if (item.Quantity == 0)
+                                {
+                                    itemsReady++;
+                                }
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("|");
+                    Console.WriteLine(roundSummary);
+                    
                 }
-            }
-            if (rowsWithSpace.Count >= width)
-            {
-                return true;
-            }
 
-            //Search for space vertically
-            for (int j = 0; j < grillSpace[0].Length; j++)
-            {
-                bool isRoomInCol = GetColumn(j).Where(cell => (cell == 0)).Count() >= length;
-                if (isRoomInCol)
-                {
-                    colsWithSpace.Add(j);
-                }
+                Console.WriteLine("------------------------------------------------------------");
+                Console.WriteLine("(Total: {0} Rounds)", rounds);
+                Console.WriteLine("------------------------------------------------------------");
+                Console.WriteLine();
             }
-            if (colsWithSpace.Count >= width)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        /// <summary>
-        /// Pretty prints the grill distribution showing the id of the element in every
-        /// cell
-        /// </summary>
-        public void PrintGrill()
-        {
-            for (int i = 0; i < 20; i++)
-            {
-                for (int j = 0; j < 30; j++)
-                {
-                    Console.WriteLine("grill[{0},{1}] = {2}", i, j, grillSpace[i][j]);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Returns column in specified index
-        /// </summary>
-        /// <param name="index">Column Index</param>
-        /// <returns>int[] with the column content</returns>
-        public int[] GetColumn(int index)
-        {
-            return grillSpace
-                .Where(o => (o != null && o.Count() > index))
-                .Select(o => o[index])
-                .ToArray();
         }
     }
 }
